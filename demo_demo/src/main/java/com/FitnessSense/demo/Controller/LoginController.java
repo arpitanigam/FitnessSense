16d5a92062ad4cf2040eb2768f1c43311036f5a8
AKANSHA <AKANSHA@10.250.3.231>
2025-09-24 19:37:17 +0100

    Structure commit

--- demo_demo/src/main/java/com/FitnessSense/demo/Controller/LoginController.java
+++ demo_demo/src/main/java/com/FitnessSense/demo/Controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.FitnessSense.demo;
+package com.FitnessSense.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- demo_demo/src/main/java/com/FitnessSense/demo/Model/DemoApplication.java
+++ demo_demo/src/main/java/com/FitnessSense/demo/Model/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.FitnessSense.demo;
+package com.FitnessSense.demo.Model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
