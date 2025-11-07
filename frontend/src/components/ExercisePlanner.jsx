b7735be36f9b02ac65c30678ab23307b3c5bad7c
Avineet Kaur <avineetkaur15@gmail.com>
2025-11-07 12:09:38 +0000

    added exercisePlanner

--- frontend/src/components/ExercisePlanner.jsx
+++ frontend/src/components/ExercisePlanner.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
 import InputForm from "./InputForm";
+import Heading from "./Heading";
+import "../styles/ExercisePlanner.css"
+import ExerciseTable from "./ExerciseTable";
 
-export default function App() {
+export default function ExercisePlanner() {
 	const [response, setResponse] = useState(null);
+	let exercises=[{
+  exercise_name: "exercise1",
+  number_of_sets: 123,
+  number_of_reps: 123,
+  video_link: "youtube.com/example",
+  day: "exampleDay"
+},
+{exercise_name: "exercise1",
+  number_of_sets: 123,
+  number_of_reps: 123,
+  video_link: "youtube.com/example",
+  day: "exampleDay"
+},{exercise_name: "exercise1",
+  number_of_sets: 123,
+  number_of_reps: 123,
+  video_link: "youtube.com/example",
+  day: "exampleDay"}
+];
 
-	if (response) {
-		return "add output table component here";
-	}
+	// if (response) {
+		return <div className="exercisePlannerdiv">
+			<Heading/>
+			<ExerciseTable exercises={exercises}/>
+		</div>;
+	//}
 
     // return form by default
-	return <InputForm setResponse={setResponse} />;
+	//return <InputForm setResponse={setResponse} />;
 }
---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Heading()
+{
+    return <div className="workoutHeader">
+        <div className="heading" >Design your workout</div>
+        <div className="goal">
+          Goal: <div><span className="goalCategory">Muscle building</span></div>
+        </div>
+    </div>
+}
