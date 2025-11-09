467f73ae107ab80c9c7505fd0b3902190c5c4b79
Arpita Nigam <nigamarpita414@gmail.com>
2025-11-09 18:17:33 +0000

    Exercise Planner Backend

--- backend/src/main/java/com/FitnessSense/demo/Controller/ExerciseTable.java
+++ backend/src/main/java/com/FitnessSense/demo/Controller/ExerciseTable.java
@@ -1,6 +1,8 @@
 package com.FitnessSense.demo.Controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +15,26 @@ import com.FitnessSense.demo.Controller.ApiController;
 public class ExerciseTable {
 	
 		@GetMapping("/get-exercise-plan")
-	    public Map<String, Object> getJsonData() {
-	        Map<String, Object> data = new HashMap<>();
-	        data.put("exercise_name", "exercise1");
-	        data.put("number_of_sets", "123");
-	        data.put("number_of_reps", "123");
-	        data.put("video_link", "youtube.com/example");
-	        data.put("day", "exampleDay");
+	    public List<Map<String, Object>> getJsonData() {
+	        Map<String, Object> obj1 = new HashMap<>();
+	        obj1.put("exercise_name", "exercise1");
+	        obj1.put("number_of_sets", "123");
+	        obj1.put("number_of_reps", "123");
+	        obj1.put("video_link", "youtube.com/example");
+	        obj1.put("day", "exampleDay");
+	        
+	        
+	        Map<String, Object> obj2 = new HashMap<>();
+	        obj2.put("exercise_name", "exercise2");
+	        obj2.put("number_of_sets", "123");
+	        obj2.put("number_of_reps", "123");
+	        obj2.put("video_link", "youtube.com/example");
+	        obj2.put("day", "exampleDay2");
+	        
+	        
+	        List<Map<String, Object>> data = new ArrayList<>();
+	        data.add(obj1);
+	        data.add(obj2);
 	        
 	        return data; 
 	    }
